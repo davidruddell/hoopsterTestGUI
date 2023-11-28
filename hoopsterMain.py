156708950e5f3e72ccd2fe1de071c0a66ca0977e
davidruddell <69439720+davidruddell@users.noreply.github.com>
2023-11-27 21:43:38 -0700

    Add files via upload

---
+++ hoopsterMain.py
@@ -0,0 +1,56 @@
+import customtkinter
+import tkinter
+from PIL import Image
+
+#root = customtkinter.CTk()
+#main.geometry("350x350")
+
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+root = customtkinter.CTk()
+root.geometry("800x480")
+root.title('Hoopster - Basketball Shooting Robot')
+
+def login():
+    print("Test")
+
+def launch_button():
+        print("launched")
+
+def set_button():
+     print("set")
+
+def abort_button():
+    print("aborted")
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+#label = customtkinter.CTkLabel(master=frame, text="HOOPSTER", font=('Courier New', 50))
+hoopsterLogo = customtkinter.CTkImage(dark_image=Image.open("images/HoopsterLogo.png"), size=(197,165.5))
+label = customtkinter.CTkLabel(master=frame, image=hoopsterLogo, text="")
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="text1")
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="text2")
+entry2.pack(pady=12, padx=10)
+
+launchButton = customtkinter.CTkButton(master=frame, text="Launch Button", command=launch_button)
+launchButton.pack(pady=12, padx=10)
+
+setButton = button = customtkinter.CTkButton(master=frame, text="Set Button", command=set_button)
+setButton.pack(pady=12, padx=10)
+
+abortButton = customtkinter.CTkButton(master=frame, text="Abort Button", command=abort_button)
+abortButton.pack(pady=12, padx=10)
+
+
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
